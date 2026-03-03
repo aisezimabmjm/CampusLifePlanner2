e837334481c7f32e33921d9dd634016e8237a8ad
Mac Air <macair@MacBook-Air-Mac.local>
2026-03-03 19:31:22 +0600

    Added completion handling, getters and toString override in PlanItem

--- src/PlanItem.java
+++ src/PlanItem.java
@@ -11,4 +11,30 @@ public class PlanItem {
         this.durationHours = durationHours;
         this.completed = false;
     }
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public int getDurationHours() {
+        return durationHours;
+    }
+
+    public int calculateUrgency() {
+        return daysUntil;
+    }
+
+    public String getDetails() {
+        return title + " | Days left: " + daysUntil;
+    }
+
+    @Override
+    public String toString() {
+        return getDetails() +
+                " | Duration: " + durationHours + "h" +
+                " | Completed: " + completed;
+    }
 }
